0391ac3c8ffb4f917ca59b9da72b765e3765ad8c
madokainagaki <jo6gencho9@gmail.com>
2022-06-06 09:36:47 +0900

    最初

---
+++ app/src/main/java/jp/techacademy/madoka/inagaki/qa_app/Const.kt
@@ -0,0 +1,13 @@
+package jp.techacademy.madoka.inagaki.qa_app
+
+//firebaseのユーザー表示名を保存するパス
+const val UsersPATH ="users"
+
+//firebaseに質問を保存するパス
+const val ContentsPATH ="contents"
+
+//firebaseに回答を保存するパス
+const val answersPATH  ="answers"
+
+//preferenceに表示名を保存する時のキー
+const val NameKEY = "name"
